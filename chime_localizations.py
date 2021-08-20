dc15e35bdc46b2e4df30d341414e315298eb4401
Jessie Thwaites <jthwaites@cobalt06.icecube.wisc.edu>
2021-08-20 15:06:14 -0500

    moved fxns to setup and localizations scripts

---
+++ chime_localizations.py
@@ -0,0 +1,83 @@
+""" Functions for computations and visualizations of Chime spatial priors
+Jessie Thwaites, 8/20/21
+"""
+
+import numpy as np
+import h5py as h5
+import healpy as hp
+import os
+
+#########################################################################
+#Globals
+chime_loc='/data/user/jthwaites/chime_localization_data/'
+chime_loc_files=os.listdir(chime_loc)
+
+#########################################################################
+#full extent of 68% contour of map, not just central lobe
+def get_new_localizations():
+    new_localizations={'tns_name':[],'ra_unc':[],'dec':[],'dec_unc':[]}
+    
+    for file in chime_loc_files:
+        f = h5.File(chime_loc+file,'r')
+    
+        ra_loc=f['/'].attrs['ra_error']
+        dec_loc=f['/'].attrs['dec_error']
+
+        extent=[[0,361],[0,91]]
+        for name, contour in f['/contours/68/'].items():
+            contour=contour[:]
+            max_ra=max(contour[0])
+            if max_ra> extent[0][0]: extent[0][0]=max_ra
+            min_ra=min(contour[0])
+            if min_ra< extent[0][1]: extent[0][1]=min_ra
+            max_dec=max(contour[1])
+            if max_dec> extent[1][0]: extent[1][0]=max_dec
+            min_dec=min(contour[1])
+            if min_dec < extent[1][1]: extent[1][1]=min_dec
+    
+        if (extent[0][0]-extent[0][1]) > ra_loc: ra_loc=extent[0][0]-extent[0][1]
+        if (extent[1][0]-extent[1][1]) > dec_loc: dec_loc=extent[1][0]-extent[1][1]
+    
+        new_localizations['tns_name'].append(f['/'].attrs['tns_name'])
+        new_localizations['dec'].append(f['/'].attrs['dec'])
+        new_localizations['ra_unc'].append(ra_loc)
+        new_localizations['dec_unc'].append(dec_loc)
+    return new_localizations
+
+############################################################################
+#make a healpix map for one frb
+def make_healpix_map(tns_name, new_nside=512, max_cl=0.90):
+    new_localizations=get_new_localizations()
+    
+    index=np.where(tns_name==np.asarray(new_localizations['tns_name']))[0][0]
+    frb = chime_loc + tns_name+'_localization.h5'
+    f = h5.File(frb, 'r')
+
+    ra = np.radians(f['/'].attrs['ra'])
+    dec = np.radians(f['/'].attrs['dec'])
+
+    original_nside = f['healpix'].attrs['nside']
+    ipix, CL = f['healpix/ipix'][()], f['healpix/CL'][()]
+    #for some reason, it appears there are duplicates of these in all the files. 
+    #To remove, use np.unqiue fxn
+    unique_CLs, CL_ind=np.unique(ipix, return_index=True)
+
+    probs = (1. - CL)/ (1. - CL[CL_ind]).sum()
+    frb_probs = np.zeros(hp.nside2npix(original_nside))
+    frb_probs[ipix] = probs
+    new_CL = np.ones_like(frb_probs)
+    new_CL[ipix] = CL
+    
+    frb_probs = hp.ud_grade(frb_probs,
+                new_nside, order_in='NESTED', order_out='RING', power=-2)
+    new_CL = hp.ud_grade(new_CL,
+                new_nside, order_in='NESTED', order_out='RING')
+
+    #get mask to use
+    msk = new_CL < max_cl
+    
+    print('sum of map probabilities: %.5f'%(frb_probs.sum()))
+    return frb_probs, msk
+
+
+        
---
+++ setup.py
@@ -0,0 +1,95 @@
+"""Setup and load datasets for FRB analysis
+Jessie Thwaites 8/20/21
+GFU, v2p6
+Define needed functions in multiple files
+"""
+
+import numpy as np
+import csky as cy
+import pandas as pd
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from astropy.time import Time
+
+ana_dir = cy.utils.ensure_dir('/home/jthwaites/csky_cache')
+ana = cy.get_analysis(cy.selections.repo, 'version-002-p06', cy.selections.GFUDataSpecs.GFU_IC86, 
+                dir=ana_dir)
+
+conf = {'extended': True, #use extended LLH due to low time window
+        'space': "ps",
+        'time': "transient",
+        'sig': 'transient',
+        'ana':ana,
+        'mp_cpus': 1 #some functions fail with >1 (?)
+        }
+cy.CONF.update(conf)
+
+#######################################################################
+#define sources for analysis (options: single or list, spatial priors
+def sources(t_s, mjd, ra, dec, prior=None, deg=True): #t_s is time window (sec)
+    if prior !=None:
+        if isinstance(mjd,list):
+            src = cy.utils.Sources(
+                ra=ra,dec=dec, mjd=[m-(t_s/(84600.*2)) for m in mjd],
+                t_100=[t_s/84600.]*len(ra), sigma_t=[0.]*len(ra),
+                prior=prior, deg=deg)
+        else:
+            src = cy.utils.Sources(
+                ra=ra,dec=dec, mjd=mjd,
+                t_100=[t_s/84600.],sigma_t=[0.],
+                prior=prior, deg=deg)
+    else: 
+        if isinstance(mjd,list):
+            src = cy.utils.Sources(
+                ra=ra,dec=dec, mjd=[m-(t_s/(84600.*2)) for m in mjd],
+                t_100=[t_s/84600.]*len(ra), sigma_t=[0.]*len(ra), deg=deg)
+        else:
+            src = cy.utils.Sources(
+                ra=ra,dec=dec, mjd=mjd,
+                t_100=[t_s/84600.],sigma_t=[0.], deg=deg)
+    return src
+
+#####################################################################        
+#load catalog csv file - default is load all if not specified
+def load_frbs(**kw):
+    if 'spatial_priors' in kw.keys(): 
+        frbs = pd.read_csv('./catalog/spatial_priors_frbs.csv')
+    elif 'frb121102' in kw.keys():
+        frbs = pd.read_csv('./catalog/frb121102_bursts.csv')
+    elif 'others' in kw.keys(): 
+        frbs = pd.read_csv('./catalog/frbs_excl_sppriors_121102.csv')
+    elif 'all' in kw.keys(): frbs = pd.read_csv('./catalog/frbs_all.csv')
+    else: 
+        frbs = pd.read_csv('./catalog/frbs_all.csv')
+        print('Loading all FRBs from catalogs')    
+    return frbs
+
+#######################################################################
+#make skymap visualization of loaded frbs
+def make_frb_skymap(frbs):
+    unique_frbs, frb_ind_all, n_frbs = np.unique(frbs['src'].values, 
+                                                 return_index=True, return_counts=True)
+    frb_ind_all=frbs['src'].index[frb_ind_all]
+        
+    unique_frbs_coord = SkyCoord(ra=frbs['ra_deg'].reindex(frb_ind_all).values, 
+                      dec=frbs['dec_deg'].reindex(frb_ind_all).values, 
+                      unit=(u.hourangle, u.deg))
+
+    ra= unique_frbs_coord.ra.rad
+    dec=unique_frbs_coord.dec.rad
+    src_map = hl.heal.hist(256, dec, ra)
+
+    fig, ax = plt.subplots(figsize=(12,15),subplot_kw=dict(projection='aitoff'))
+    sp = cy.plotting.SkyPlotter(pc_kw=dict(cmap='winter',vmin=0))
+    mesh, cb = sp.plot_map(ax, np.where(src_map.map>0, src_map.map, np.nan), n_ticks=2)
+    kw = dict(color='.5', alpha=.5)
+    sp.plot_gp(ax, lw=.5, **kw)
+    sp.plot_gc(ax, **kw)
+    ax.grid(**kw)
+    plt.title('Skymap of FRB locations \n')
+    cb.set_label(r'FRBs/bin')
+    
+    if print_plot==False: plt.show()
+    else: plt.savefig('./plots/frb_skymap.png')
+        
+
